0f1bcb647acdd1796be5ddbb380395b8f383b937
mirzachairat <mirzachairat5@gmail.com>
2023-05-15 16:00:48 +0700

    perbaikan title login

--- config/leaflet.php
+++ config/leaflet.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'zoom_level'           => 13,
-    'detail_zoom_level'    => 16,
+    'zoom_level'           => 10,
+    'detail_zoom_level'    => 10,
     'map_center_latitude' => env('MAP_CENTER_LATITUDE', '-6.120000'),
     'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '106.150276'),
 ];
--- config/leafletsetup.php
+++ config/leafletsetup.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'zoom_level' => 13,
+    'zoom_level' => 10,
     'detail_zoom_level' => 10,
     'map_center_latitude' => env('MAP_CENTER_LATITUDE', '-6.120000'),
     'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '106.150276'),
