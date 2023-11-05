2a0b0a05cc4190ad9aa5c5d50818e814fee14509
mirzachairat <mirzachairat5@gmail.com>
2023-11-06 02:58:24 +0700

    font style lading page judul di rubah

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -110,10 +110,10 @@
                         <div class="row">
                             <div class="col-lg-7 order-1 order-lg-0">
                                 <div class="text-area">
-                                    <h1 class="wow fadeInUp" style="font-size:24px" data-wow-delay="0.2s" data-wow-duration="1s">PELAYANAN DAN PENGUJIAN VETERINER</h1>
-                                    <h1 class="wow fadeInUp" style="font-size:48px" data-wow-delay="0.4s" data-wow-duration="1s">DINAS PERTANIAN</h1>
-                                    <h1 class="wow fadeInUp" style="font-size:48px" data-wow-delay="0.4s" data-wow-duration="1s">PROVINSI BANTEN</h1>
-                                    <p class="wow fadeInUp" style="font-size:20px; font-color:blue;" data-wow-delay="0.6s" data-wow-duration="1s">Selamat Datang di Web Layanan Online Veteriner Dinas Pertanian Provinsi Banten(Love Banten)</p>
+                                    <p class="wow fadeInUp" style="font-size:15px" data-wow-delay="0.2s" data-wow-duration="1s">PELAYANAN DAN PENGUJIAN VETERINER</p>
+                                    <p class="wow fadeInUp" style="font-size:35px" data-wow-delay="0.4s" data-wow-duration="1s">DINAS PERTANIAN</p>
+                                    <p class="wow fadeInUp" style="font-size:35px" data-wow-delay="0.4s" data-wow-duration="1s">PROVINSI BANTEN</p>
+                                    <p class="wow fadeInUp" style="font-size:15px; font-color:white;" data-wow-delay="0.6s" data-wow-duration="1s">Selamat Datang di Web Layanan Online Veteriner Dinas Pertanian Provinsi Banten(Love Banten)</p>
                                     <ul class="d-flex wow fadeInUp" data-wow-delay="0.8s" data-wow-duration="1s">
                                         <li>
                                             <a href="https://lovebanten.com/login" type="button" style="width:200px" class="btn btn-outline-light btn-lg">Login</a>
